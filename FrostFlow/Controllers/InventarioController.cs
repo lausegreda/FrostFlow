9a568b32f798e03c9464066ef513d7df054cb4ae
Keylin Ramírez <107515516+Keylinramirez@users.noreply.github.com>
2024-04-08 02:14:33 -0600

    Paginas Creadas
    Paginas de Inventario y Ordenes de trabajo

---
+++ FrostFlow/Controllers/InventarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostFlow.Controllers
+{
+    public class InventarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult BuscadorProducto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegistrarProducto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Notificaciones()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Informes()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FrostFlow/Controllers/OrdenTrabajoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostFlow.Controllers
+{
+    public class OrdenTrabajoController : Controller
+    {
+        [HttpGet]
+        public IActionResult OrdenesTrabajo()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Notificaciones()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AsignacionTecnico()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TecnicoOT()
+        {
+            return View();
+        }
+    }
+}
