dc0359cfe19a254ccb538f35cf486ce5dc9c71a8
lausegreda <laura.segreda@gmail.com>
2024-04-08 21:02:35 -0600

    Planillas Completo

---
+++ FrostFlow/Controllers/PlanillaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostFlow.Controllers
+{
+    public class PlanillaController : Controller
+    {
+        public IActionResult RegistrarEmpleado()
+        {
+            return View();
+        }
+
+        public IActionResult MantenimientoEmpleado()
+        {
+            return View();
+        }
+
+        public IActionResult GenerarPlanilla()
+        {
+            return View();
+        }
+    }
+}
